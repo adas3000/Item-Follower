b513a8a1e663d208059e01a96d90ff83b1441a54
Adam_Z <youngtoss38@gmail.com>
2020-02-25 10:44:30 +0100

    Update gradle deps

--- app/src/main/java/pl/allegro/follower/view/MainActivity.kt
+++ app/src/main/java/pl/allegro/follower/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package pl.allegro.follower
+package pl.allegro.follower.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pl.allegro.follower.R
 
 class MainActivity : AppCompatActivity() {
 
