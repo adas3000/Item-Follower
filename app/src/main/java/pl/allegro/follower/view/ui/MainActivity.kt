110c86d126283824e5bef4f55bbaffc2231f22eb
Adam_Z <youngtoss38@gmail.com>
2020-02-26 12:28:50 +0100

    Add itemtouchhelper to recycler view

--- app/src/main/java/pl/allegro/follower/view/ui/MainActivity.kt
+++ app/src/main/java/pl/allegro/follower/view/ui/MainActivity.kt
@@ -9,7 +9,9 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 import pl.allegro.follower.R
@@ -52,6 +54,22 @@ class MainActivity : AppCompatActivity() {
         recyclerView_item_list.setHasFixedSize(true)
         recyclerView_item_list.layoutManager = LinearLayoutManager(this)
         recyclerView_item_list.adapter = adapter
+
+        ItemTouchHelper(object:ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                itemViewModel.delete(adapter.getItemAtPosition(viewHolder.adapterPosition))
+            }
+        }
+        ).attachToRecyclerView(recyclerView_item_list)
+
     }
 
 
